befcd0075a4de4453f8d6824090e778c00b0f9e3
Heverson Vasconcelos <heverson.vasconcelos@nastek.com.br>
2021-12-28 16:53:49 -0400

    Arquitetura inicial replicando dados da tabela gw_sensor

--- postgres/init-db.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-  CREATE DATABASE test_db;
-  \c test_db;
-  \i home/setup.sql;
-  \i home/fake.sql;
--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
+#!/bin/sh
 # setup connections
-#curl -i -X POST -H "Accept:application/json" -H  "Content-Type:application/json" http://localhost:8083/connectors/ -d @reqs/connections/source.json
-curl -i -X POST -H "Accept:application/json" -H  "Content-Type:application/json" http://localhost:8083/connectors/ -d @reqs/connections/es-sink-users.json
-curl -i -X POST -H "Accept:application/json" -H  "Content-Type:application/json" http://localhost:8083/connectors/ -d @reqs/connections/es-sink-posts.json
-curl -i -X POST -H "Accept:application/json" -H  "Content-Type:application/json" http://localhost:8083/connectors/ -d @reqs/connections/es-sink-comments.json
+curl -i -X POST -H "Accept:application/json" -H  "Content-Type:application/json" http://localhost:8083/connectors/ --data @reqs/connections/source.json
+curl -i -X POST -H "Accept:application/json" -H  "Content-Type:application/json" http://localhost:8083/connectors/ --data @reqs/connections/es-sink-gw_sensor.json
+curl -i -X PUT -H "Accept:application/json" -H  "Content-Type:application/json" http://localhost:9200/_ingest/pipeline/ --data @reqs/pipelines/convert-sensor-location.json
