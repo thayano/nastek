08f8897514835bec3fae350af0f9fbe626e3759f
Heverson Vasconcelos <heverson.vasconcelos@nastek.com.br>
2022-01-06 17:51:37 -0400

    Simplificação das dependências

--- 1-es-sink-mapppings-connectors.sh
+++ 1-es-sink-mapppings-connectors.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 # setup connections
-#curl -i -X POST -H "Accept:application/json" -H  "Content-Type:application/json" http://localhost:8083/connectors/ --data @reqs/connections/es-sink-gw_sensor.json
-#curl -i -X POST -H "Accept:application/json" -H  "Content-Type:application/json" http://localhost:8083/connectors/ --data @reqs/connections/es-sink-gw_data.json
+curl -i -X POST -H "Accept:application/json" -H  "Content-Type:application/json" http://localhost:8083/connectors/ --data @reqs/connections/es-sink-gw_sensor.json
+curl -i -X POST -H "Accept:application/json" -H  "Content-Type:application/json" http://localhost:8083/connectors/ --data @reqs/connections/es-sink-gw_data.json
 
 # create indices with mappings
-curl -i -X PUT -H "Accept:application/json" -H  "Content-Type:application/json" http://localhost:9200/gw_sensor/_mapping --data @reqs/mappings/gw_sensor.json
+#curl -i -X PUT -H "Accept:application/json" -H  "Content-Type:application/json" http://localhost:9200/gw_sensor/_mapping --data @reqs/mappings/gw_sensor.json
 
 # delete connections
 #curl -X DELETE http://localhost:8083/connectors/db_teste_elk-connector
