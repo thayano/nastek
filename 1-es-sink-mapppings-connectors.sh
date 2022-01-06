9326c5517098df30c453a03ba900b8037aeca834
Heverson Vasconcelos <heverson.vasconcelos@nastek.com.br>
2022-01-06 10:14:00 -0400

    Simplificação da topologia e arquitetura

--- 1-es-sink-mapppings-connectors.sh
+++ 1-es-sink-mapppings-connectors.sh
@@ -1,18 +1,15 @@
 #!/bin/sh
 
+# setup connections
+#curl -i -X POST -H "Accept:application/json" -H  "Content-Type:application/json" http://localhost:8083/connectors/ --data @reqs/connections/es-sink-gw_sensor.json
+#curl -i -X POST -H "Accept:application/json" -H  "Content-Type:application/json" http://localhost:8083/connectors/ --data @reqs/connections/es-sink-gw_data.json
+
 # create indices with mappings
 curl -i -X PUT -H "Accept:application/json" -H  "Content-Type:application/json" http://localhost:9200/gw_sensor/_mapping --data @reqs/mappings/gw_sensor.json
 
-# setup connections
-#curl -i -X POST -H "Accept:application/json" -H  "Content-Type:application/json" http://localhost:8083/connectors/ --data @reqs/connections/es-sink-gw_data.json
-#curl -i -X POST -H "Accept:application/json" -H  "Content-Type:application/json" http://localhost:8083/connectors/ --data @reqs/connections/es-sink-gw_sensor.json
-
 # delete connections
 #curl -X DELETE http://localhost:8083/connectors/db_teste_elk-connector
-#curl -X DELETE http://localhost:8083/connectors/es-sink-sensor
+#curl -X DELETE http://localhost:8083/connectors/es-sink-gw_data
 #curl -X DELETE http://localhost:8083/connectors/es-sink-gw_sensor
-
-# setup pipelines
-#curl -i -X PUT -H "Accept:application/json" -H  "Content-Type:application/json" http://localhost:9200/_ingest/pipeline/ --data @reqs/pipelines/convert-sensor-location.json
 
 
