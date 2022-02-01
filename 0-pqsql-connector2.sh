d545616325a0aaf7d0c1498d2f59de0331a73049
ttdavalo <ttdavalo@gmail.com>
2022-02-01 17:29:04 +0000

    Projeto owl

---
+++ 0-pqsql-connector2.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# setup connections
+curl -i -X POST -H "Accept:application/json" -H  "Content-Type:application/json" http://localhost:8083/connectors/ --data @reqs/connections/gw_source2.json
+
+# delete connections
+#curl -X DELETE http://localhost:8083/connectors/db_teste_elk-connector
--- 1-es-sink-connectors.sh
+++ 1-es-sink-connectors.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # setup connections
-curl -i -X POST -H "Accept:application/json" -H  "Content-Type:application/json" http://localhost:8083/connectors/ --data @reqs/connections/es-sink-gw_sensor.json
-curl -i -X POST -H "Accept:application/json" -H  "Content-Type:application/json" http://localhost:8083/connectors/ --data @reqs/connections/es-sink-gw_data.json
+#curl -i -X POST -H "Accept:application/json" -H  "Content-Type:application/json" http://localhost:8083/connectors/ --data @reqs/connections/es-sink-access.json
+curl -i -X POST -H "Accept:application/json" -H  "Content-Type:application/json" http://localhost:8083/connectors/ --data @reqs/connections/es-sink-gw_data2.json
 
 # delete connections
 #curl -X DELETE http://localhost:8083/connectors/es-sink-gw_data
